41bcfc60209a9a90c92e928d7a8bf2c29fb47d5e
Prateek Singhal <m22aie215@iitj.ac.in>
2023-09-01 22:55:04 +0530

    Assignment 2 - 3 way split and predict & eval functions

--- exp.py
+++ exp.py
@@ -8,14 +8,11 @@ hand-written digits, from 0-9.
 
 """
 
-# Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
-# License: BSD 3 clause
+# Author: Prateek Singhal
 
 # Standard scientific Python imports
-import matplotlib.pyplot as plt
 
 # Import datasets, classifiers and performance metrics
-from sklearn import metrics
 import utils
 
 
@@ -52,36 +49,47 @@ X, y = utils.read_digits()
 # in the test subset.
 
 # 2. Data splitting -- to create train and test sets
-# Split data into 50% train and 50% test subsets
-X_train, X_test, y_train, y_test = utils.split_data(X, y, test_size=0.3)
+X_train, X_dev, X_test, y_train, y_dev, y_test = utils.split_train_dev_test(
+    X, y, test_size=0.2, dev_size=0.3
+)
 
 # 3. Data preprocessing
 X_train = utils.preprocess_data(X_train)
+X_dev = utils.preprocess_data(X_dev)
 X_test = utils.preprocess_data(X_test)
 
 # 4. Model initialization and model fit
 # Create a classifier: a support vector classifier
-
-
 model = utils.train_model(X_train, y_train, {"gamma": 0.001})
 
-# 5. Prediction on test data
-# Predict the value of the digit on the test subset
-predicted = model.predict(X_test)
+# 5. Predict and Evaluation:
+accuracy = utils.predict_and_eval(model, X_dev, y_dev)
+if accuracy < 0.8:
+    raise ValueError("Change parameters to get higher accuracy than 80%.")
 
-# 6. Evaluation
+
+# 6. Testing on Test data:
+print("Testing model for Robustness")
+test_accuracy = utils.predict_and_eval(model, X_dev, y_dev)
 print(
-    f"Classification report for classifier {model}:\n"
-    f"{metrics.classification_report(y_test, predicted)}\n"
+    f"""
+Validation and Test accuracies of the model are:
+validation accuracy {accuracy}
+test accuracy   {test_accuracy}
+"""
 )
+
 
 ###############################################################################
 # We can also plot a :ref:`confusion matrix <confusion_matrix>` of the
 # true digit values and the predicted digit values.
 
-disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
-disp.figure_.suptitle("Confusion Matrix")
-print(f"Confusion matrix:\n{disp.confusion_matrix}")
+# disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
+# disp.figure_.suptitle("Confusion Matrix")
+
+
+# cm = metrics.confusion_matrix(y_test, predicted)
+# print(f"Confusion matrix:\n{cm}")
 
 # plt.show()
 
@@ -93,18 +101,18 @@ print(f"Confusion matrix:\n{disp.confusion_matrix}")
 
 
 # The ground truth and predicted lists
-y_true = []
-y_pred = []
-cm = disp.confusion_matrix
+# y_true = []
+# y_pred = []
+# # cm = disp.confusion_matrix
 
-# For each cell in the confusion matrix, add the corresponding ground truths
-# and predictions to the lists
-for gt in range(len(cm)):
-    for pred in range(len(cm)):
-        y_true += [gt] * cm[gt][pred]
-        y_pred += [pred] * cm[gt][pred]
+# # For each cell in the confusion matrix, add the corresponding ground truths
+# # and predictions to the lists
+# for gt in range(len(cm)):
+#     for pred in range(len(cm)):
+#         y_true += [gt] * cm[gt][pred]
+#         y_pred += [pred] * cm[gt][pred]
 
-print(
-    "Classification report rebuilt from confusion matrix:\n"
-    f"{metrics.classification_report(y_true, y_pred)}\n"
-)
+# print(
+#     "Classification report rebuilt from confusion matrix:\n"
+#     f"{metrics.classification_report(y_true, y_pred)}\n"
+# )
--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 from sklearn.model_selection import train_test_split
-from sklearn import svm, datasets
+from sklearn import svm, datasets, metrics
 
 """ 
 Common functions:
@@ -22,6 +22,27 @@ def split_data(X, y, test_size, random_state=1):
     return X_train, X_test, y_train, y_test
 
 
+def split_train_dev_test(X, y, test_size, dev_size, random_state=1):
+    test_dev_size = test_size + dev_size
+    if test_dev_size > 0.5:
+        raise ValueError(
+            f"Total test and Dev data cannot be more than 50% of entire data"  # noqa
+        )
+    X_train, X_test_dev, y_train, y_test_dev = train_test_split(
+        X,
+        y,
+        test_size=test_dev_size,
+        random_state=random_state,
+    )
+    X_test, X_dev, y_test, y_dev = train_test_split(
+        X_test_dev,
+        y_test_dev,
+        test_size=dev_size / test_dev_size,
+        random_state=random_state,
+    )
+    return X_train, X_dev, X_test, y_train, y_dev, y_test
+
+
 def train_model(x, y, model_params, model_type="svm"):
     if model_type == "svm":
         clf = svm.SVC
@@ -33,3 +54,15 @@ def train_model(x, y, model_params, model_type="svm"):
 def read_digits():
     digits = datasets.load_digits()
     return digits.images, digits.target
+
+
+def predict_and_eval(model, X_test, y_test):
+    predicted = model.predict(X_test)
+    cm = metrics.confusion_matrix(y_test, predicted)
+    print(f"Confusion matrix:\n{cm}")
+    print(
+        f"Classification report for classifier {model}:\n"
+        f"{metrics.classification_report(y_test, predicted)}\n"
+    )
+    accuracy = metrics.accuracy_score(y_test, predicted)
+    return accuracy
