67aa9d4cb880bf7c5dbd1de894bc28a475ff6bbb
Prateek Singhal <m22aie215@iitj.ac.in>
2023-09-23 14:32:26 +0530

    Q2-Quiz

--- exp.py
+++ exp.py
@@ -7,10 +7,12 @@ This example shows how scikit-learn can be used to recognize images of
 hand-written digits, from 0-9.
 
 """
+
 # Author: Prateek Singhal
 
 
 import itertools
+from skimage.transform import resize, rescale
 
 import utils
 
@@ -46,7 +48,39 @@ print(f"Total Samples -> {X.shape[0]}")
 print(f"Shape of the images in (height, width) -> {X.shape[1:]}")
 print()
 
-# QUIZ STATEMENTS
+
+shapes = [(4, 4), (6, 6), (8, 8)]
+for shape in shapes:
+    div = round(X.shape[1] / shape[0], 4)
+    new_X = [rescale(x, div, anti_aliasing=True) for x in X]
+    size_grid = {"test_size": [0.2], "dev_size": [0.1]}
+    combinations = utils.get_combinations_with_keys(size_grid)
+    for combination in combinations:
+        test_size = combination["test_size"]
+        dev_size = combination["dev_size"]
+        train_size = 1 - (test_size + dev_size)
+        if train_size <= 0:
+            raise ValueError("Sum of test and dev should be less than 1")
+        (
+            X_train,
+            X_dev,
+            X_test,
+            y_train,
+            y_dev,
+            y_test,
+        ) = utils.split_train_dev_test(new_X, y, test_size, dev_size)
+        # X_train = utils.preprocess_data(X_train)
+        # X_dev = utils.preprocess_data(X_dev)
+        # X_test = utils.preprocess_data(X_test)
+        best_model, best_params, dev_accuracy = utils.tune_hparams(
+            X_train, X_dev, y_train, y_dev, h_params_grid
+        )
+        train_accuracy = utils.predict_and_eval(best_model, X_train, y_train)
+        test_accuracy = utils.predict_and_eval(best_model, X_test, y_test)
+        # print("Optimal parameters: ", best_params)
+        print(
+            f"image size={shape} test_size={test_size} dev_size={dev_size} train_size={train_size} train_acc={train_accuracy} dev_acc={dev_accuracy} test_acc={test_accuracy}"  # noqa
+        )
 
 
 ###############################################################################
@@ -76,34 +110,37 @@ print()
 # X_test = utils.preprocess_data(X_test)
 
 
-size_grid = {"test_size": [0.1, 0.2, 0.3], "dev_size": [0.1, 0.2, 0.3]}
-combinations = utils.get_combinations_with_keys(size_grid)
-for combination in combinations:
-    test_size = combination["test_size"]
-    dev_size = combination["dev_size"]
-    train_size = 1 - (test_size + dev_size)
-    if train_size <= 0:
-        raise ValueError("Sum of test and dev should be less than 1")
-    (
-        X_train,
-        X_dev,
-        X_test,
-        y_train,
-        y_dev,
-        y_test,
-    ) = utils.split_train_dev_test(X, y, test_size, dev_size)
-    X_train = utils.preprocess_data(X_train)
-    X_dev = utils.preprocess_data(X_dev)
-    X_test = utils.preprocess_data(X_test)
-    best_model, best_params, dev_accuracy = utils.tune_hparams(
-        X_train, X_dev, y_train, y_dev, h_params_grid
-    )
-    train_accuracy = utils.predict_and_eval(best_model, X_train, y_train)
-    test_accuracy = utils.predict_and_eval(best_model, X_test, y_test)
-    # print("Optimal parameters: ", best_params)
-    print(
-        f"test_size={test_size} dev_size={dev_size} train_size={train_size} train_acc={train_accuracy} dev_acc={dev_accuracy} test_acc={test_accuracy}"  # noqa
-    )
+# size_grid = {"test_size": [0.1, 0.2, 0.3], "dev_size": [0.1, 0.2, 0.3]}
+
+
+# size_grid = {"test_size": [0.2], "dev_size": [0.1]}
+# combinations = utils.get_combinations_with_keys(size_grid)
+# for combination in combinations:
+#     test_size = combination["test_size"]
+#     dev_size = combination["dev_size"]
+#     train_size = 1 - (test_size + dev_size)
+#     if train_size <= 0:
+#         raise ValueError("Sum of test and dev should be less than 1")
+#     (
+#         X_train,
+#         X_dev,
+#         X_test,
+#         y_train,
+#         y_dev,
+#         y_test,
+#     ) = utils.split_train_dev_test(X, y, test_size, dev_size)
+#     X_train = utils.preprocess_data(X_train)
+#     X_dev = utils.preprocess_data(X_dev)
+#     X_test = utils.preprocess_data(X_test)
+#     best_model, best_params, dev_accuracy = utils.tune_hparams(
+#         X_train, X_dev, y_train, y_dev, h_params_grid
+#     )
+#     train_accuracy = utils.predict_and_eval(best_model, X_train, y_train)
+#     test_accuracy = utils.predict_and_eval(best_model, X_test, y_test)
+#     # print("Optimal parameters: ", best_params)
+#     print(
+#         f"test_size={test_size} dev_size={dev_size} train_size={train_size} train_acc={train_accuracy} dev_acc={dev_accuracy} test_acc={test_accuracy}"  # noqa
+#     )
 
 
 # HYPER PARAMETERS TUNING
