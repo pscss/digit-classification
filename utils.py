f5fd3af30598d01a461f9a519edb74d1466e8d19
Prateek Singhal <m22aie215@iitj.ac.in>
2023-10-07 14:20:20 +0530

    support for tree classifier

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 from sklearn.model_selection import train_test_split
-from sklearn import svm, datasets, metrics
+from sklearn import svm, tree, datasets, metrics
 from sklearn.model_selection import ParameterGrid
 from joblib import dump
 import itertools
@@ -48,6 +48,8 @@ def split_train_dev_test(X, y, test_size, dev_size, random_state=1):
 def train_model(x, y, model_params, model_type="svm"):
     if model_type == "svm":
         clf = svm.SVC
+    elif model_type == "tree":
+        clf = tree.DecisionTreeClassifier
     model = clf(**model_params)
     model.fit(x, y)
     return model
@@ -76,20 +78,20 @@ def get_combinations_with_keys(grid):
     return [dict(zip(keys, combination)) for combination in combinations]
 
 
-def tune_hparams(X_train, X_dev, y_train, y_dev, h_params_grid):
+def tune_hparams(X_train, X_dev, y_train, y_dev, h_params_grid, model_type):
     best_accuracy = -1
     best_model = None
     best_params = {}
 
     for h_params in ParameterGrid(h_params_grid):
-        cur_model = train_model(X_train, y_train, h_params, model_type="svm")
+        cur_model = train_model(X_train, y_train, h_params, model_type)
         cur_accuracy = predict_and_eval(cur_model, X_dev, y_dev)
 
         if cur_accuracy > best_accuracy:
             best_accuracy = cur_accuracy
             best_params = h_params
             best_model = cur_model
-            best_model_path = f'./models/best_model_{"_".join([f"{k}-{v}" for k, v in best_params.items()])}.joblib'  # noqa
-            dump(best_model, best_model_path)
+    best_model_path = f'./models/best_model_{"_".join([f"{k}-{v}" for k, v in best_params.items()])}.joblib'  # noqa
+    dump(best_model, best_model_path)
 
     return best_model_path, best_params, best_accuracy
