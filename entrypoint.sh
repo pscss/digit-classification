cb93fc5bd4436d4bdc46cb076446482d3ba6d1ac
Prateek Singhal <m22aie215@iitj.ac.in>
2023-11-07 21:41:57 +0530

    adding compare feature

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 pytest
 
 # Run your Python script with hyperparameters
-# python exp.py --hyperparameters hyperparameters.json
+python exp.py --hyperparameters hyperparameters.json
 
 
-python main.py
+# python main.py
--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-# from flask import Flask
-
-# app = Flask(__name__)
-
-
-# @app.route("/")
-# def hello_world():
-#     return "<p>Hello, World!</p>"
+from PIL import Image
+import numpy as np
+from utils import preprocess_data
+from joblib import load
 
 
-from flask import Flask, render_template, request
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
 
+# @app.route("/")
+# def hello_world():
+#     return render_template("template.html")
+
+
 @app.route("/")
-def hello_world():
-    return render_template("template.html")
+def index():
+    return render_template("compare_image.html")
 
 
 @app.route("/sum/<x>/<y>")
@@ -34,6 +35,33 @@ def pred_model():
     return f"<p>sum is {result}</p>"
 
 
+@app.route("/compare_images", methods=["POST"])
+def compare_images():
+    try:
+        image1 = Image.open(request.files["image1"])
+        image2 = Image.open(request.files["image2"])
+
+        # Preprocess the images and convert them to numpy arrays
+        image1 = np.array(image1.resize((8, 8)))
+        image2 = np.array(image2.resize((8, 8)))
+
+        # Flatten the images to make them compatible with the classifier
+        image1 = image1.reshape(1, -1)
+        image2 = image2.reshape(1, -1)
+
+        model_path = "models/best_model_C-1_gamma-0.001.joblib"
+        svm = load(model_path)
+        digit1 = svm.predict(image1)
+        digit2 = svm.predict(image2)
+
+        # Compare the predicted digits
+        are_same = digit1[0] == digit2[0]
+
+        return jsonify({"are_same": are_same})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+
 if __name__ == "__main__":
     print("server is running")
-    app.run(host="0.0.0.0", port=5000)
+    app.run()
